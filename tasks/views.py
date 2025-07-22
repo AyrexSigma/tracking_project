ce5da80729ece06a25dd7d1f7399bba00c631295
AyrexSigma <ayrex.ytz@gmail.com>
2025-07-22 15:55:52 +0300

    git create update1

---
+++ tasks/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
+from .models import Task
+from .forms import TaskForm
+
+# Create your views here.
+
+class TaskListView(ListView):
+    model = Task
+    template_name = 'tasks/task_list.html'
+
+class TaskDetailView(DetailView):
+    model = Task
+    template_name = 'tasks/task_detail.html'
+
+class TaskCreateView(CreateView):
+    model = Task
+    form_class = TaskForm
+    template_name = 'tasks/task_form.html'
+    success_url = '/tasks/'
